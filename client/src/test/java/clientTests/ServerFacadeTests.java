0b7decdcfdb81436caf04647b2b829e3362e02a0
Brayden <Brayden@smarty.com>
2024-03-27 22:28:02 -0600

    some more fun tests!

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,6 +1,8 @@
 package clientTests;
 
+import dataAccess.DataAccessException;
 import org.junit.jupiter.api.*;
+import server.ResponseException;
 import server.Server;
 import server.UnauthorizedException;
 import ui.ChessClientFacade;
@@ -41,7 +43,7 @@ public class ServerFacadeTests {
     }
 
     @Test
-    public void login() {
+    public void loginTest() {
         String[] params = new String[]{"Willie", "Wonka", "itsASecret@wizbang.com"};
 
         assertDoesNotThrow(() -> {
@@ -50,12 +52,176 @@ public class ServerFacadeTests {
     }
 
     @Test
-    public void loginNonExistentUser() {
-        String[] params = new String[]{"John", "The Spy", "shhh@shush.com"};
+    public void loginNonExistentUserTest() {
+        String[] params = new String[]{"James", "The Spy", "shhh@shush.com"};
 
         assertThrows(UnauthorizedException.class, () -> {
-            clientFacade.login(finalToken);
+            clientFacade.login(params);
         });
     }
+
+    @Test
+    public void registerTest(){
+        int suffix = (int)(Math.random() * 10000);
+        String[] params = new String[]{"John" + suffix, "The Spy", "shhh@shush.com"};
+
+        assertDoesNotThrow(() -> {
+            clientFacade.register(params);
+        });
+    }
+
+    @Test
+    public void registerMalformedUserTest(){
+        String[] params = new String[]{"John", "The Spy"};
+
+        assertThrows(DataAccessException.class, () -> {
+            clientFacade.register(params);
+        });
+    }
+
+    @Test
+    public void logoutTest(){
+        String[] params = new String[]{"Willie", "Wonka", "itsASecret@wizbang.com"};
+
+        assertDoesNotThrow(() -> {
+            clientFacade.login(params);
+        });
+
+        assertDoesNotThrow(() -> {
+            clientFacade.logout();
+        });
+    }
+
+    @Test
+    public void logoutNotSignedInTest(){
+        assertThrows(ResponseException.class, () -> {
+            clientFacade.logout();
+        });
+    }
+
+    @Test
+    public void createGameTest(){
+        String[] params = new String[]{"Willie", "Wonka", "itsASecret@wizbang.com"};
+
+        assertDoesNotThrow(() -> {
+            clientFacade.login(params);
+        });
+
+        int suffix = (int)(Math.random() * 10000);
+
+        String[] params2 = new String[]{"game" + suffix};
+
+        assertDoesNotThrow(() -> {
+            clientFacade.createGame(params2);
+        });
+    }
+
+    @Test
+    public void createGameBadInputTest(){
+        assertThrows(DataAccessException.class, () -> {
+            clientFacade.createGame();
+        });
+    }
+
+    @Test
+    public void listGamesTest(){
+        String[] params = new String[]{"Willie", "Wonka", "itsASecret@wizbang.com"};
+
+        assertDoesNotThrow(() -> {
+            clientFacade.login(params);
+        });
+
+        assertDoesNotThrow(() -> {
+            clientFacade.listGames();
+        });
+    }
+
+    @Test
+    public void listGamesNotSignedInTest(){
+        assertThrows(DataAccessException.class, () -> {
+            clientFacade.listGames();
+        });
+    }
+
+    public static String extractID(String str) {
+        // Find the index of the last space character
+        int lastSpaceIndex = str.lastIndexOf(" ");
+
+        // Extract the substring following the last space
+        if (lastSpaceIndex != -1 && lastSpaceIndex + 1 < str.length()) {
+            return str.substring(lastSpaceIndex + 1);
+        }
+        return "";
+    }
+
+    @Test
+    public void joinGameTest(){
+        String[] params = new String[]{"Willie", "Wonka", "itsASecret@wizbang.com"};
+
+        assertDoesNotThrow(() -> {
+            clientFacade.login(params);
+        });
+
+        String[] params2 = new String[]{"Willie's game"};
+        String id = "";
+
+        try{
+            String joinString = clientFacade.createGame(params2);
+            id = extractID(joinString);
+        }catch(Exception e){
+            System.out.println("advise: " + e);
+        }
+
+        String[] params3 = new String[]{id, "black"};
+        assertDoesNotThrow(() -> {
+            clientFacade.joinGame(params3);
+        });
+    }
+
+    @Test
+    public void joinGameBadIdTest(){
+        String[] params = new String[]{"Willie", "Wonka", "itsASecret@wizbang.com"};
+
+        assertDoesNotThrow(() -> {
+            clientFacade.login(params);
+        });
+
+        assertThrows(DataAccessException.class, () -> {
+            clientFacade.joinGame();
+        });
+    }
+
+    @Test
+    public void joinObserverTest(){
+        String[] params = new String[]{"Willie", "Wonka", "itsASecret@wizbang.com"};
+
+        assertDoesNotThrow(() -> {
+            clientFacade.login(params);
+        });
+
+        String[] params2 = new String[]{"Willie's game"};
+        String id = "";
+
+        try{
+            String joinString = clientFacade.createGame(params2);
+            id = extractID(joinString);
+        }catch(Exception e){
+            System.out.println("advise: " + e);
+        }
+
+        String[] params3 = new String[]{id};
+        assertDoesNotThrow(() -> {
+            clientFacade.joinObserver(params3);
+        });
+    }
+
+    @Test
+    public void joinObserverNoIdTest(){
+        assertThrows(DataAccessException.class, () -> {
+            clientFacade.joinObserver();
+        });
+    }
+
+
 
 }
